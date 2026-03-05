ede1761a74fda4fbfe2380e59c6b4de15f837252
AnuragBhattacharjee04 <bhattacharjeeanurag000@gmail.com>
2026-03-05 23:29:30 +0530

    Mongoose setup and schema

--- index.js
+++ index.js
@@ -7,6 +7,15 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+const mongoose = require("mongoose");
+const strokeSchema = new mongoose.Schema({
+    points: [{ x: Number, y: Number }],
+    color: String,
+    timestamp: { type: Date, default: Date.now }
+});
+
+const Stroke = mongoose.model('Stroke', strokeSchema);
+
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
