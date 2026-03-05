74f1f6484dc52d1f2dddc633b372d1da1fdcd6ed
AnuragBhattacharjee04 <bhattacharjeeanurag000@gmail.com>
2026-03-05 23:09:25 +0530

    Socket.io setup

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const express = require("express");
-const http = require("http");
-const {server} = require('socket.io');
-const path = require('path')
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+const PORT = 6000;
+server.listen(PORT, () => console.log(`Server running at ${PORT}`));
