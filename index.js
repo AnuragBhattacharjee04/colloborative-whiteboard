06b1141625a6f38e5fe95cc5fecaf511cefa7c46
AnuragBhattacharjee04 <bhattacharjeeanurag000@gmail.com>
2026-03-05 23:12:12 +0530

    EJS setup

--- index.js
+++ index.js
@@ -7,5 +7,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
+
 const PORT = 6000;
 server.listen(PORT, () => console.log(`Server running at ${PORT}`));
